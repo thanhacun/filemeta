9ced578490032a59c81b5178eadfc57b04e4d181
THCN <thanhacun@gmail.com>
2016-04-05 11:30:35 +0000

    beta

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,30 @@
 'use strict';
+//https://www.terlici.com/2015/05/16/uploading-files-locally.html
+//http://stackoverflow.com/questions/27785896/use-multer-in-express-route-using-meanjs
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var multer = require('multer');
+var filesize = require('filesize');
+
+var uploading = multer({
+	dest: path + '/public/upload',
+	limits: {filesize: 1000000, files: 1}
+});
 
 module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
-
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+	
+	app.post('/upload', uploading.single('aFile'), function(req, res) {
+		//console.log(req.file);
+		res.json({
+			'filename': req.file.originalname,
+			'filesize': filesize(req.file.size)
 		});
+	});
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+	
 };
